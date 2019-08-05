041543d85c1c329b6f66e72a12c04c53aded765f
zhenging218 <zhenging218@gmail.com>
2019-08-05 17:32:58 +0800

    WIP GPU minimax

--- source/gpubitboard.cuh
+++ source/gpubitboard.cuh
@@ -2,25 +2,49 @@
 
 #include "bitboard.h"
 #include "minimax.h"
-#include <helper_cuda.h>
+#include <thrust/device_vector.h>
 
 namespace Checkers
 {
 	struct GPUBitBoard
 	{
 		using board_type = BitBoard::board_type;
-		using utility_type = Minimax::utility_type;
+		using count_type = BitBoard::count_type;
+		
+		static constexpr board_type L3Mask = BitBoard::L3Mask;
+		static constexpr board_type L5Mask = BitBoard::L5Mask;
+		static constexpr board_type R3Mask = BitBoard::R3Mask;
+		static constexpr board_type R5Mask = BitBoard::R5Mask;
+
+		static constexpr board_type OddRows = BitBoard::OddRows;
+
+		static constexpr board_type BlackKingMask = BitBoard::BlackKingMask;
+		static constexpr board_type WhiteKingMask = BitBoard::WhiteKingMask;
 
 		board_type white, black, kings;
-		bool valid;
 
 		__host__ __device__ GPUBitBoard();
-		__host__ __device__ GPUBitBoard(board_type w, board_type b, board_type k, bool v);
+		__host__ __device__ GPUBitBoard(board_type w, board_type b, board_type k);
 		__host__ __device__ GPUBitBoard &operator=(GPUBitBoard const &src);
 
 		__host__ GPUBitBoard(BitBoard const &src);
 		__host__ GPUBitBoard &operator=(BitBoard const &src);
 
 		__host__ operator BitBoard() const;
+
+		__device__ static bool GetPossibleWhiteMoves(GPUBitBoard const &b, thrust::device_vector<GPUBitBoard> &dst);
+		__device__ static bool GetPossibleBlackMoves(GPUBitBoard const &b, thrust::device_vector<GPUBitBoard> &dst);
+
+		__device__ static board_type GetBlackMoves(BitBoard const &b);
+		__device__ static board_type GetWhiteMoves(BitBoard const &b);
+
+		__device__ static board_type GetBlackJumps(BitBoard const &b);
+		__device__ static board_type GetWhiteJumps(BitBoard const &b);
+
+		__device__ static count_type GetBlackPieceCount(BitBoard const &b);
+		__device__ static count_type GetWhitePieceCount(BitBoard const &b);
+
+		__device__ static count_type GetBlackKingsCount(BitBoard const &b);
+		__device__ static count_type GetWhiteKingsCount(BitBoard const &b);
 	};
 }
---
+++ source/precomp.cuh
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "gpubitwise_helpers.cuh"
+#include "gpubitboard.cuh"
+#include "gpuminimax.cuh"
