1d7547842c4b1fe7872c0939059d3e373fe31ffd
zhenging218 <zhenging218@gmail.com>
2019-08-06 16:40:25 +0800

    updated pseudokernels for tree-split kernel impls

--- pseudocode/pseudokernels.h
+++ pseudocode/pseudokernels.h
@@ -1,167 +1,158 @@
-using gen_func = GPUBitBoard(*)(GPUBitBoard const *, GPUBitBoard::board_type);
-
-using gen_jump_func = GPUBitBoard(*)(GPUBitBoard const *, GPUBitBoard::board_type, GPUBitBoard::board_type *);
-
-__device__ GPUBitBoard GenWhiteMovesUL(GPUBitBoard const *b, GPUBitBoard::board_type i)
+__device__ GenWhiteMoves(GPUBitBoard::board_type cell, GPUBitBoard *out, GPUBitBoard const *board)
 {
-	if(position & 0xF0F0F0F0u) // odd
-	{
-		if(((i & b->kings) << 4) & empty)
-		{
-			return GPUBitBoard((b->white & ~i) | (i << 4), b->black, (b->kings & ~i) | (i << 4));
-		}
-	}
-	else
-	{
-		if((((i & L3Mask) & b->kings) << 3) & empty)
-		{
-			return GPUBitBoard((b->white & ~i) | (i << 3), b->black, (b->kings & ~i) | (i << 3));
-		}
-	}
+	// GPUBitBoard must have a validity boolean.
 }
 
-__device__ GPUBitBoard GenWhiteJumpsUL(GPUBitBoard const *b, GPUBitBoard::board_type i, GPUBitBoard::board_type *i2)
+__device__ GenBlackMoves(GPUBitBoard::board_type cell, GPUBitBoard *out, GPUBitBoard const *board)
 {
-	if(position & 0xF0F0F0F0u) // odd
-	{
-		if (((i & b.kings) << 4) & b.black) //UL from odd
-		{
-			board_type j = i << 4;
-			if (((j & L3Mask) << 3) & empty) // UL from even
-			{
-				*i2 = j << 3;
-				return GPUBitBoard((b.white & ~i) | (j << 3), (b.black & ~j), (b.kings & (~i ^ j)) | (j << 3));
-			}
-		}
-	}
-	else
-	{
-		if ((((i & L3Mask) & b.kings) << 3) & b.black) //UL
-		{
-			board_type j = i << 3;
-			if ((j << 4) & empty)
-			{
-				*i2 = j << 4;
-				return GPUBitBoard((b.white & ~i) | (j << 4), (b.black & ~j), (b.kings & (~i ^ j)) | (j << 4));
-			}
-
-
-		}
-	}
+	// GPUBitBoard must have a validity boolean.
 }
 
-__device__ gen_func GenWhiteMoves = { GenWhiteMovesUL, GenWhiteMovesUR, GenWhiteMovesLL, GenWhiteMovesLR };
-__device__ gen_func GenWhiteJumps = { GenWhiteJumpsUL, GenWhiteJumpsUR, GenWhiteJumpsLL, GenWhiteJumpsLR };
+// will need master_black_max_kernel, master_black_min_kernel, black_max_kernel and black_min_kernel as well.
+// non_master kernels will create 4 streams to use on its own per thread.
 
-__device__ gen_func GenBlackMoves = { GenBlackMovesUL, GenBlackMovesUR, GenBlackMovesLL, GenBlackMovesLR };
-__device__ gen_func GenBlackJumps = { GenBlackJumpsUL, GenBlackJumpsUR, GenBlackJumpsLL, GenBlackJumpsLR };
-
-__global__ void EvaluateMoreWhiteJumpsKernel(GPUBitBoard::utility_type *utility, GPUBitBoard const *src, GPUBitBoard::board_type i, int depth, int turns_left)
+__global__ master_white_max_kernel(GPUBitBoard::utility_type *v, GPUBitBoard::utility_type *utility, GPUBitBoard const *src, int num_boards, int alpha, int beta, int depth, int turns)
 {
-	__shared__ GPUBitBoard *moves;
-	
-	int tx = threadIdx.x;
-	
-	GPUBitBoard board_type jumps = GPUBitBoard::GetWhiteJumps(src);
-	
-	if(jumps)
-	{
-		if(tx == 0)
-		{
-			moves = cudaMalloc(sizeof(GPUBitBoard) * 4);
-		}
-		
-		__syncthreads();
-		
-		GPUBitBoard::board_type i2;
-		moves[tx] = GetWhiteJumps[tx](src, /*  shift i according to tx (4 directions, based on odd/even row) */, &i2);
-		EvaluateMoreWhiteJumps(/* utility return pointer */, moves[tx], i2, depth, turns_left);
-	}
-	else
-	{
-		// run next tree
-		dim3 dimGrid(4, 1, 1);
-		dim3 dimBlock(32, 1, 1);
-		
-		GPUBitBoard::utility_type *utility = cudaMalloc(sizeof(GPUBitBoard::utility_type));
-		
-		EvaluateWhiteMin<<<dimGrid, dimBlock>>>(utility, );
-	}
-}
-
-__device__ void EvaluateMoreWhiteJumps(GPUBitBoard::utility_type *utility, GPUBitBoard const *src, GPUBitBoard::board_type i, int depth, int turns_left)
-{
-	dim3 dimGrid(1,1,1);
-	dim3 dimBlock(4,1,1);
-	EvaluateMoreWhiteJumpsKernel<<<dimGrid, dimBlock>>>(utility, src, i, depth, turns_left);
-}
-
-__global__ void StartMinimaxWhite(bool *valid, GPUBitBoard *dst, GPUBitBoard const *src, GPUBitBoard::board_type position, int depth, int turns_left)
-{
-	__shared__ GPUBitBoard *moves;
-	
 	int tx = threadIdx.x;
 	int index = blockIdx.x * blockDim.x + tx;
 	
-	GPUBitBoard board_type moves = GPUBitBoard::GetWhiteMoves(src);
-	GPUBitBoard board_type jumps = GPUBitBoard::GetWhiteJumps(src);
+	// master kernel doesn't need terminal test because the CPU function that runs this kernel won't run kernel if terminal test terminates that node.
 	
-	if(jumps)
+	if(index < num_boards)
 	{
-		if(tx == 0)
-		{
-			moves = cudaMalloc(sizeof(GPUBitBoard) * 32);
-		}
-		__syncthreads();
-		
-		if(jumps & (1u << tx))
-		{
-			GPUBitBoard::board_type i2;
-			moves[tx] = GenWhiteJumps[blockIdx.x](src, 1u << tx, &i2);
-			// jump again (device function)
-			EvaluateMoreWhiteJumps(/* utility return pointer */, moves[tx], i2, depth, turns_left);
-			
-		}
-		
-		__syncthreads();
-		
-		if(tx == 0)
-		{
-			cudaFree(moves);
-			valid = true;
-		}
-		
-		__syncthreads();
+		// CPU will cudaMalloc the utility array for the master kernel.
+		// CPU will initialize all the utility values to the first v on the leftmost tree that was evaluated by the CPU.
+		white_min_kernel<<<dim3(1,1,1), dim3(32,1,1)>>>(utility + index, src + index, alpha, beta, depth - 1, turns - 1);
 	}
-	else if(moves)
+	
+	__syncthreads();
+	
+	if(index == 0)
 	{
-		if(tx == 0)
+		// do ab-pruning
+		for(int i = 0; i < num_boards; ++i)
 		{
-			moves = cudaMalloc(sizeof(GPUBitBoard) * 32);
+			*v = max(utility[index], *v);
+			if(*v > beta)
+			{
+				break;
+			}
 		}
-		__syncthreads();
-		
-		if(moves & (1u << tx))
+		alpha = max(alpha, *v);
+	}
+	
+	__syncthreads();
+}
+
+__global__ master_white_min_kernel(GPUBitBoard::utility_type *v, GPUBitBoard::utility_type *utility, GPUBitBoard const *src, int num_boards, int alpha, int beta, int depth, int turns)
+{
+	// same logic as master_white_max_kernel, but use min node logic
+}
+
+__global__ white_min_kernel(GPUBitBoard::utility_type *v, GPUBitBoard const *src, int alpha, int beta, int depth, int turns)
+{
+	int tx = threadIdx.x;
+	int t_beta = beta;
+	__shared__ bool terminated;
+	__shared__ GPUBitBoard::utility_type utilities[32];
+
+	if(tx < 32)
+	{
+		utilities[tx] = *v;
+	}
+	
+	__syncthreads();
+
+	if(tx == 0)
+	{
+		GPUBitBoard::utility_type terminal_value = 0;
+		if(src->valid)
 		{
-			moves[tx] GenWhiteMoves[blockIdx.x](src, 1u << tx);
-			// evaluate next player
+			*v = Infinity;
+			terminated = GPUGetWhiteUtility(src, &terminal_value, depth, turns);
+			if(terminated)
+			{
+				*v = terminal_value;
+			}
 		}
-		
-		__syncthreads();
-		
-		if(tx == 0)
+		else
 		{
-			cudaFree(moves);
-			valid = true;
+			terminated = true;
 		}
-		
-		__syncthreads();
+	}
+	
+	__syncthreads();
+	
+	if(terminated)
+	{
+		return;
 	}
 	else
 	{
+		GPUBitBoard::utility_type *utility = cudaMalloc(sizeof(GPUBitBoard::utility_type) * 4);
+		utility[0] = utility[1] = utility[2] = utility[3] = utilities[tx];
+		GPUBitBoard *new_boards = cudaMalloc(sizeof(GPUBitBoard) * 4);
+		
+		cudaStream_t streams[4];
+		cudaStreamCreateWithFlags(&streams[0], cudaStreamNonBlocking);
+		cudaStreamCreateWithFlags(&streams[1], cudaStreamNonBlocking);
+		cudaStreamCreateWithFlags(&streams[2], cudaStreamNonBlocking);
+		cudaStreamCreateWithFlags(&streams[3], cudaStreamNonBlocking);
+		
+		GenBlackMoves(1u << tx, new_board, src);
+		white_max_kernel<<<dim3(1,1,1), dim3(32,1,1), streams[0]>>>(utility, new_boards[0], t_alpha, t_beta, depth - 1, turns - 1);
+		white_max_kernel<<<dim3(1,1,1), dim3(32,1,1), streams[1]>>>(utility + 1, new_boards[1], alpha, t_beta, depth - 1, turns - 1);
+		white_max_kernel<<<dim3(1,1,1), dim3(32,1,1), streams[2]>>>(utility + 2, new_boards[2], alpha, t_beta, depth - 1, turns - 1);
+		white_max_kernel<<<dim3(1,1,1), dim3(32,1,1), streams[3]>>>(utility + 3, new_boards[3], alpha, t_beta, depth - 1, turns - 1);
+		
+		// sync streams here
+		
+		for(int i  = 0; i < 4; ++i)
+		{
+			cudaStreamSynchronize(streams[i]);
+			utilities[tx] = min(utility[i], utilities[tx]);
+			if(utilities[tx] < alpha)
+			{
+				break;
+			}
+			else
+			{
+				t_beta = min(utilities[tx], t_beta);
+			}
+		}
+		
+		cudaFree(utility);
+		cudaFree(new_boards);
+		
+		cudaStreamDestroy(streams[0]);
+		cudaStreamDestroy(streams[1]);
+		cudaStreamDestroy(streams[2]);
+		cudaStreamDestroy(streams[3]);
+		
+		__syncthreads();
+		
 		if(tx == 0)
 		{
-			*valid = false;
+			// final ab-pruning for this node
+			for(int i = 0; i < 32; ++i)
+			{
+				*v = min(utilities[i], *v);
+				if(*v < alpha)
+				{
+					break;
+				}
+				else
+				{
+					beta = min(utilities[i], beta);
+				}
+			}
 		}
+		
+		__syncthreads();
 	}
+}
+
+__global__ white_max_kernel(GPUBitBoard::utility_type *v, GPUBitBoard const *src, int alpha, int beta, int depth, int turns)
+{
+	// same as white_min_kernel, but use max logic.
 }
